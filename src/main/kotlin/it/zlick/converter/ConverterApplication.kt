65ee0258783b04dfa6015a87574401d363cfedf4
Abdul Fatah <abdulfatah66@gmail.com>
2021-03-28 02:35:05 +0500

    refactor configuration

--- src/main/kotlin/it/zlick/converter/ConverterApplication.kt
+++ src/main/kotlin/it/zlick/converter/ConverterApplication.kt
@@ -1,17 +1,14 @@
 package it.zlick.converter
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import it.zlick.converter.service.TransactionService
 import org.apache.logging.log4j.LogManager
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
@@ -22,21 +19,11 @@ class ConverterApplication {
   @Bean
   fun restTemplate(objectMapper: ObjectMapper): RestTemplate {
     val restTemplate = RestTemplate(HttpComponentsClientHttpRequestFactory())
-
     restTemplate.messageConverters.add(0, MappingJackson2HttpMessageConverter(objectMapper))
     return restTemplate
   }
 
-  @Bean
-  @Primary
-  fun objectMapper(): ObjectMapper {
-    val objectMapper = ObjectMapper().registerModule(JavaTimeModule()).registerModule(KotlinModule())
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-
-    return objectMapper
-  }
-
+  @Order(Ordered.LOWEST_PRECEDENCE)
   @Bean
   fun clr(service: TransactionService) = CommandLineRunner {
     val summary = service.process(n=1, chunkSize = 10, targetCurrency = "EUR");
