c1c78ac7ca8f9c6c67bcf26679be22d917b84f8c
Abdul Fatah <abdulfatah66@gmail.com>
2021-03-29 15:00:47 +0500

    use coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-json")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.javamoney:moneta:1.4.1")
--- src/main/kotlin/it/zlick/converter/service/impl/TransactionServiceImpl.kt
+++ src/main/kotlin/it/zlick/converter/service/impl/TransactionServiceImpl.kt
@@ -1,18 +1,18 @@
 package it.zlick.converter.service.impl
 
-import it.zlick.converter.model.Transaction
-import it.zlick.converter.service.Result
-import it.zlick.converter.service.Summary
 import it.zlick.converter.service.TransactionConverter
 import it.zlick.converter.service.TransactionService
+import it.zlick.converter.service.external.ProcessResult
 import it.zlick.converter.service.external.TransactionProcessor
 import it.zlick.converter.service.external.TransactionProvider
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CompletableFuture.allOf
-import java.util.concurrent.CompletableFuture.supplyAsync
 
 @Service
 class TransactionServiceImpl(
@@ -24,90 +24,27 @@ class TransactionServiceImpl(
   @Value("\${processor.chunk-size}")
   private val chunkSize = 10
 
-  override fun process(n: Int, targetCurrency: String): Summary {
-    val fetchAsyncs = fetchTransactions(n)
-    val convertAsyncs = convertTransactions(fetchAsyncs, targetCurrency)
+  override fun process(n: Int, targetCurrency: String) {
+    val chunks = n / chunkSize
+    val results = mutableListOf<ProcessResult>()
 
-    allOf(*convertAsyncs.toTypedArray()).thenApply {
-      convertAsyncs.map { it.join() }
-    }
-
-    val fetched = fetchAsyncs.map { it.get() }.filterNotNull()
-    val converted = convertAsyncs.map { it.get() }.filterNotNull()
-    val notConverted = fetched.filter { it.checksum !in converted.map { it.checksum } }
-
-    val processAsyncs = processTransactions(converted, chunkSize)
-
-    allOf(*processAsyncs.toTypedArray()).thenApply {
-      processAsyncs.map { it.join() }
-    }
-
-    val processingResults = processAsyncs.map { it.get() }
-
-    return Summary(
-      expected = n,
-      fetched = fetched.size,
-      conversion = Result(
-        expected = fetched.size,
-        successful = converted.size,
-        failed = fetched.size - converted.size,
-        failures = notConverted
-      ),
-      processing = processingResults.reduce { acc, result ->
-        acc.copy(
-          expected = acc.expected + result.expected,
-          successful = acc.successful + result.successful,
-          failed = acc.failed + result.failed,
-          failures = acc.failures.plus(result.failures)
-        )
-      }
-    )
-  }
-
-  private fun fetchTransactions(n: Int): List<CompletableFuture<Transaction?>> {
-    return (1..n).map {
-      supplyAsync {
-        runCatching {
-          provider.getTransaction()
-        }.recover {
-          LOG.error("Transaction could not be fetched: ${it.message}")
-          null
-        }.getOrNull()
-      }
-    }
-  }
-
-  private fun convertTransactions(transactions: List<CompletableFuture<Transaction?>>, targetCurrency: String): List<CompletableFuture<Transaction?>> {
-    return transactions.map {
-      if(it != null) {
-        it.thenCompose {
-          supplyAsync {
-            runCatching {
-              converter.convert(it!!, targetCurrency)
-            }.recover {
-              LOG.error("Transaction could not be converted: ${it.message}")
-              null
-            }.getOrNull()
+    runBlocking {
+      (1..chunks).forEach {
+        launch {
+          (1..chunkSize).asFlow().map {
+            provider.getTransaction()
+          }.map {
+            converter.convert(it, targetCurrency)
+          }.toList().also {
+            val result = processor.process(it)
+            results.add(result)
           }
         }
-      } else {
-        CompletableFuture.completedFuture(null)
-      }
-    }
-  }
 
-  private fun processTransactions(list: List<Transaction>, chunkSize: Int): List<CompletableFuture<Result>> {
-    return list.chunked(chunkSize) { chunk ->
-      supplyAsync {
-        runCatching {
-          val result = processor.process(chunk)
-          Result(expected = chunk.size, successful = chunk.size, failed = 0, failures = emptyList())
-        }.recover {
-          LOG.error("${it.message}")
-          Result(expected = chunk.size, successful = 0, failed = chunk.size, failures = chunk)
-        }.getOrNull()
       }
     }
+
+    LOG.info(results)
   }
 
   companion object {
